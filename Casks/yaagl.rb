c6a326f3cef7655eb5de2ca84214f03c8034fd56
HuaDeity <hello@huadeity.com>
2024-03-05 22:50:27 +0800

    yaagl 0.1.8

--- Casks/yaagl.rb
+++ Casks/yaagl.rb
@@ -1,6 +1,6 @@
 cask "yaagl" do
-  version "0.1.7"
-  sha256 "82fa7f1743bff51df7bf38058c5cff190aa1660a5a72234871efd6f2d5219166"
+  version "0.1.8"
+  sha256 "0c09576cd2be241bf806f3c15d0de6a74ea92d63dcec47aec27bbdd2836c99e1"
 
   url "https://github.com/3Shain/yet-another-anime-game-launcher/releases/download/#{version}/Yaagl.app.tar.gz"
   name "Yaagl"
