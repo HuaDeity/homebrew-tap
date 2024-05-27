12a3b375789b642c480416b318b1302d899d3203
HuaDeity <hello@huadeity.com>
2024-05-27 13:25:19 +0800

    Add Cask

---
+++ Casks/yaagl-hsr-os.rb
@@ -0,0 +1,25 @@
+cask "yaagl-hsr-os" do
+  version "0.1.11"
+  sha256 "ab0764bc4c28fc702aa3b18eb6ac4884202f03231aace00d98d94dff130d77de"
+
+  url "https://github.com/yaagl/yet-another-anime-game-launcher/releases/download/#{version}/Yaagl.HSR.OS.app.tar.gz"
+  name "Yaagl HSR OS"
+  desc "Yet another anime game launcher for HSR OS Version"
+  homepage "https://github.com/yaagl/yet-another-anime-game-launcher/"
+
+  livecheck do
+    url :url
+  end
+
+  auto_updates true
+  depends_on macos: ">= :high_sierra"
+
+  app "Yaagl HSR OS.app"
+
+  zap trash: [
+    "~/Library/Application Support/Yaagl OS",
+    "~/Library/Caches/com.3shain.yaagl.os",
+    "~/Library/Saved Application State/com.3shain.yaagl.os.savedState",
+    "~/Library/WebKit/com.3shain.yaagl.os",
+  ]
+end
---
+++ Casks/yaagl-hsr.rb
@@ -0,0 +1,25 @@
+cask "yaagl-hsr" do
+  version "0.1.11"
+  sha256 "966c7e9efebee7331956f027f9df5699c14e237e9623fdf008068c57b144b4cc"
+
+  url "https://github.com/yaagl/yet-another-anime-game-launcher/releases/download/#{version}/Yaagl.HSR.app.tar.gz"
+  name "Yaagl HSR"
+  desc "Yet another anime game launcher for HSR"
+  homepage "https://github.com/yaagl/yet-another-anime-game-launcher/"
+
+  livecheck do
+    url :url
+  end
+
+  auto_updates true
+  depends_on macos: ">= :high_sierra"
+
+  app "Yaagl HSR.app"
+
+  zap trash: [
+    "~/Library/Application Support/Yaagl",
+    "~/Library/Caches/com.3shain.yaagl",
+    "~/Library/Saved Application State/com.3shain.yaagl.savedState",
+    "~/Library/WebKit/com.3shain.yaagl",
+  ]
+end
