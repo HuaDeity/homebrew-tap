ee0bdbd670bc58236c564bcbc092c2a768d84d8f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-24 12:54:17 +0000

    yaagl-hsr 0.2.22

--- Casks/yaagl-hsr.rb
+++ Casks/yaagl-hsr.rb
@@ -1,6 +1,6 @@
 cask "yaagl-hsr" do
-  version "0.2.21"
-  sha256 "395526b5789457687e48e7a47647fb62495ea40352fa1f2e2b7d43dfdd4d743c"
+  version "0.2.22"
+  sha256 "a3da426b13074c25b8e6ba40b5365dc3ed312920a01000882e5009abeb5bca24"
 
   url "https://github.com/yaagl/yet-another-anime-game-launcher/releases/download/#{version}/Yaagl.HSR.app.tar.gz"
   name "Yaagl HSR"
