d77ed06ccdc0e76a43bf331b4a73e76baed6b247
HuaDeity <hello@huadeity.com>
2026-01-31 22:55:29 +0800

    yaagl-os 0.3.8

--- Casks/yaagl-os.rb
+++ Casks/yaagl-os.rb
@@ -1,6 +1,6 @@
 cask "yaagl-os" do
-  version "0.3.0"
-  sha256 "047d94e0232ea81a770ac97878deeb9800677bf581e67e2384e0f9b30b9eb5c2"
+  version "0.3.8"
+  sha256 "3357fee2341bf73bc11999031dd40a0380db69fea023ae0bd64f075761608d6c"
 
   url "https://github.com/yaagl/yet-another-anime-game-launcher/releases/download/#{version}/Yaagl.OS.app.tar.gz"
   name "Yaagl OS"
@@ -12,7 +12,7 @@ cask "yaagl-os" do
   end
 
   auto_updates true
-  depends_on macos: ">= :high_sierra"
+  depends_on macos: ">= :catalina"
 
   app "Yaagl OS.app"
 
