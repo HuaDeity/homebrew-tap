6fdc0c932b320557f42c4189cefddb36bbc2fba6
Yizun Wang <64831499+HuaDeity@users.noreply.github.com>
2026-04-04 17:11:53 +0000

    yaagl-zzz 0.3.12

--- Casks/yaagl-zzz.rb
+++ Casks/yaagl-zzz.rb
@@ -1,6 +1,6 @@
 cask "yaagl-zzz" do
-  version "0.3.8"
-  sha256 "6b6cb7956df34c84386a8d54bb9bae8f79f51f6808b5619a2d1e137f37a86030"
+  version "0.3.12"
+  sha256 "b5f6f24a39afb71d02c132c132c2a0b6e3d03423fdab6fd7c8edc0aacf108d78"
 
   url "https://github.com/yaagl/yet-another-anime-game-launcher/releases/download/#{version}/Yaagl.ZZZ.app.tar.gz"
   name "Yaagl ZZZ"
