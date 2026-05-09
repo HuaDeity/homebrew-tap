2580bbf73240b6ef2e4e3cfe81c91ba6ab9bce0d
Yizun Wang <64831499+HuaDeity@users.noreply.github.com>
2026-05-09 08:38:04 +0000

    package-version-server: add 0.0.10 bottle.

--- Formula/package-version-server.rb
+++ Formula/package-version-server.rb
@@ -11,6 +11,15 @@ class PackageVersionServer < Formula
     strategy :github_latest
   end
 
+  bottle do
+    root_url "https://ghcr.io/v2/huadeity/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "f639979a635a276b8cff5b17b9ff98dfa420f78cfb60b4f31710fefa43871e3c"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "3a730c3ba9919fef8184d284f5ad6d312bc1b3994f72eb06db934d68cbbaa5d5"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "157c3d05bbf1f548ecc3400aa8fb115e1c312cca896d776b0676c2d5dcadabf2"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "8523c1dd0fee8b5ea9fdce47eeb721d1e1779230dcd8271457b069d3ec25bba3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "577592d99ed9093a4077eee7282a5a193940c385cbd3a02b7c193fc4919756b0"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "rust" => :build
 
